d92e466e9ca5ab7e07a06ca9493865e30136e9ee
ickabot <ipoole@bu.edu>
2018-08-31 11:42:58 -0400

    Updted scripts and function scripts
    Updated all functions and scripts

--- Functionsforspendcube.R
+++ Functionsforspendcube.R
@@ -1,3 +1,9 @@
+library(tidyverse)
+library(magrittr)
+library(lubridate)
+library(readr)
+library(readxl)
+library(stringr)
 
 Add_Transaction_ID <- function(df, dfsource, column, dropcolumn = TRUE) {
   ### this function is to create a unique transaction id for
@@ -7,9 +13,6 @@ Add_Transaction_ID <- function(df, dfsource, column, dropcolumn = TRUE) {
   ### the last two items need to be quoted "" character strings
   ### dfsource is the source of that df to be used in the unique id
   ### column is the column that will be used to generate unique ids
-  library(tidyverse)
-  library(magrittr)
-  library(lubridate)
   a<-df
   a %<>% mutate(trans_id = paste0(dfsource,"_",str_replace_all(as.character(today()),"-",""),match(df[[column]],
                                                         unique(df[[column]]))))
@@ -28,8 +31,42 @@ Add_Transaction_ID <- function(df, dfsource, column, dropcolumn = TRUE) {
 #droppedcolumn<-Add_Transaction_ID(Dat, "SRM", "site")
 #keptcolumn<-Add_Transaction_ID(Dat, "SRM", "site", FALSE)
 
+### function to clean the column names
+
 CleanColnames <- function(df) {
+  colnames(df) <- str_replace_all(colnames(df),"#", "Num")
+  colnames(df) <- str_replace_all(colnames(df),"$", "Spend")
   colnames(df) <- str_replace_all(colnames(df),"[[:punct:]]", "")
   colnames(df) <- str_replace_all(colnames(df)," ","_")
   colnames(df) <- tolower(colnames(df))
+}
+
+### function to create column types where you suppply number of character and number of digit columns
+
+ColTypesdigitend <- function(n_character_cols,n_digit_cols) {
+  paste0(paste0(rep("c",n_character_cols),collapse = ""),
+         paste0(rep("d",n_digit_cols),collapse = ""),
+         collapse = "")
+}
+
+### function to create column names for BW reports
+
+BWColnames <- function (directory) {
+  rm_spend <- c("$","#")
+  colstr1 <- as.character(read_delim(directory[1],skip = 5, delim = ";", n_max = 1, col_names = FALSE))
+  colstr1 <- colstr1[!colstr1 %in% rm_spend]
+  colstr2 <- na.omit(as.character(read_delim(directory[1],skip = 4, delim = ";", n_max = 1, col_names = FALSE))) 
+  c(colstr1,colstr2)
+}
+
+### combined import function for BW files
+
+BWImport <- function(directories,x,y) {
+  coltypes <- ColTypesdigitend(x,y)
+  colnames <- BWColnames(directories)
+  importtable <- bind_rows(lapply(directories,read_delim, delim = ";", 
+                                  col_types = coltypes, 
+                                  col_names = colnames, skip = 5))
+  colnames(importtable) <- CleanColnames(importtable)
+  importtable
 }
--- MonthlyMetrics.R
+++ MonthlyMetrics.R
@@ -1,43 +1,107 @@
-library(tidyverse)
-library(magrittr)
-library(stringr)
-library(lubridate)
-library(readr)
-library(readxl)
+
+### loads packages and functions
+setwd("C:/Users/ipoole/Desktop/Spend Cube/spend-cube")
+source("Functionsforspendcube.R")
+
+### Sets the wd as in the data sources directory
+setwd("Data_Sources/")
 
 ### Sets up the DocOutPut Import
-setwd("C:/Users/ipoole/Desktop/Spend Cube/spend-cube/Data Sources/")
 setwd("1_SRM/MM_1_DOCOUTPUT")
 dir_docoutput <- dir()
 
 ### defines all docoutput column types as charcter
 
-docoutputcolumntypes<-paste0(rep("c",11),collapse = "")
+coltypes_docoutput <- paste0(rep("c",11),collapse = "")
 
 ### Imports all docoutputs over eachother
 
-t_docoutput <- bind_rows(lapply(dir_docoutput,read_tsv,col_types = docoutputcolumntypes))
+t_docoutput <- bind_rows(lapply(dir_docoutput,read_tsv,col_types = coltypes_docoutput))
 
 ### Cleans the column names
 
 colnames(t_docoutput)<-CleanColnames(t_docoutput)
 
-### creates the PO version number
+### Makes created_on a date field
 
-t_docoutput %<>% group_by(document_number) %>%
-  mutate(po_version = row_number(document_number),
-         created_on = mdy(created_on))
+t_docoutput$created_on <- mdy(t_docoutput$created_on)
 
-t_docoutput_2 <- t_docoutput %>% group_by(document_number) %>%
-  mutate(created_on = mdy(created_on)) %>%
-  arrange(created_on) %>%
-  slice(1L)
+### filters change and non-change POs
 
+t_docoutput_changePOs <- t_docoutput %>%
+  filter(duplicated(document_number)|duplicated(document_number,fromLast = TRUE))
 
-### Sets up the PO Details import
+t_docoutput_nochangePOs <- t_docoutput %>%
+  filter(!duplicated(document_number)&!duplicated(document_number,fromLast = TRUE)) %>%
+  mutate(po_version = 1, original_created_on = created_on)
+
+### creates the PO version number for all Changed POs
+
+t_docoutput_changePOs %<>% group_by(document_number) %>%
+  mutate(po_version = row_number(document_number))
+
+### creates the PO original Created on Date
+
+t_docoutput_changePOs_v1 <- t_docoutput_changePOs %>%
+  filter(po_version == 1) %>%
+  mutate(original_created_on = created_on)
+
+t_docoutput_cpov1_dateselect <- t_docoutput_changePOs_v1 %>%
+  select(document_number, original_created_on)
+
+t_docoutput_changePOs_vup <- t_docoutput_changePOs %>%
+  filter(po_version != 1) %>%
+  left_join(t_docoutput_cpov1_dateselect)
+
+t_docouput_f1 <- bind_rows(t_docoutput_changePOs_v1,t_docoutput_changePOs_vup,t_docoutput_nochangePOs)
+
+### cleans up the current environment before starting the next part
+
+rm(t_docoutput_cpov1_dateselect, t_docoutput_changePOs_v1,
+   t_docoutput_changePOs, t_docoutput_nochangePOs, t_docoutput,
+   t_docoutput_changePOs_vup)
+
+### Imports and appends all PO details
 setwd("..")
 setwd("MM_2_PODETAILS")
 dir_podetails <- dir()
 
 t_podetails <- bind_rows(lapply(dir_podetails,read_xlsx))
 
+### sets wd for PO Statuses
+setwd("..")
+setwd("MM_4_POSTATUS")
+dir_postatus <- dir()
+
+### creates the column types for po_status
+
+coltypes_postatus <- ColTypesdigitend(13,2)
+
+### creates the column names for po_status
+
+colnames_postatus <- BWColnames(dir_postatus)
+
+### imports the postatus files
+
+t_postatus <- bind_rows(lapply(dir_postatus,read_delim, delim = ";", 
+                               col_types = coltypes_postatus, 
+                               col_names = colnames_postatus, skip = 5))
+
+### cleans the postatus column names
+
+colnames(t_postatus) <- CleanColnames(t_postatus)
+
+### sets wd for shopping carts
+setwd("..")
+setwd("MM_3_SCEXTRACT")
+dir_scextract <- dir()
+
+dir_scextract_new <- dir_scextract[startsWith(dir_scextract,"sc")]
+dir_scextract_old <- dir_scextract[!startsWith(dir_scextract,"sc")]
+
+scextractold<-read_delim(dir_scextract_old,delim = "|") %>%
+  select(1:30)
+
+colnames(scextractold) <- CleanColnames(scextractold)
+
+scextract_new <- lapply()
